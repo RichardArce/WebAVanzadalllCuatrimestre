3edaaebfedfc5a7ad73d5079a8dc4c8b54439137
RichardArce <richard.arce.vargas@gmail.com>
2026-09-14 20:00:25 -0600

    Add project files.

---
+++ Controllers/DuennoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAVanzadalllCuatrimestre.Controllers
+{
+    public class DuennoController : Controller
+    {
+
+        //private readonly IDuennoService _duennoService;
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ListarDuenno()
+        {
+            //var duenno = await _duennoService.ListarDuenno();
+
+            return Json("ok");
+        }
+
+        public async Task<IActionResult> GuardarDuenno(/*Duenno dto*/)
+        {
+            //var duenno = await _duennoService.GuardarDuenno(dto);
+            return Json("ok");
+        }
+
+        public async Task<IActionResult> EditarDuenno(/*Duenno dto*/)
+        {
+            //var duenno = await _duennoService.EditarDuenno(dto);
+            return Json("ok");
+        }
+
+        public async Task<IActionResult> EliminarDuenno(int id)
+        {
+            //var duenno = await _duennoService.EliminarDuenno(id);
+            return Json("ok");
+        }
+
+
+
+
+
+    }
+}
